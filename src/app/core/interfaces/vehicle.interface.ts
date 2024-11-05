391b96fd770885b07f7f78e0a5957bb96e4161d6
CristinaOsorio <macry_08@hotmail.com>
2024-11-05 01:23:06 -0600

    feat: add create to vehicle service

--- src/app/core/interfaces/vehicle.interface.ts
+++ src/app/core/interfaces/vehicle.interface.ts
@@ -4,18 +4,17 @@ export interface BaseVehicle {
     vin: string;
     model: string;
     license_plate: string;
+    status: VehicleStatus;
 }
 
 export interface Vehicle extends BaseVehicle {
     id: number;
-    status: VehicleStatus;
     created_at: string;
     updated_at: string;
 }
 
 export interface UpdateVehicle extends BaseVehicle {
     id: number;
-    status: VehicleStatus;
 }
 
 export interface CreateVehicle {}
--- src/app/core/services/vehicle.service.ts
+++ src/app/core/services/vehicle.service.ts
@@ -1,16 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { CreateVehicle, Vehicle } from '../interfaces/vehicle.interface';
+import { ApiResponse } from '../interfaces/api-response.interface';
 
 @Injectable({
     providedIn: 'root',
 })
 export class VehicleService {
-    private apiUrl = `${environment.apiUrl}/vehicles`;
     http = inject(HttpClient);
 
-    getVehicles(): Observable<any> {
-        return this.http.get(this.apiUrl);
+    private apiUrl = `${environment.apiUrl}/vehicles`;
+    private httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+        }),
+    };
+
+    getVehicles() {
+        return this.http.get<ApiResponse<Vehicle[]>>(this.apiUrl);
+    }
+
+    create(vehicle: CreateVehicle): Observable<ApiResponse<Vehicle>> {
+        return this.http.post<ApiResponse<Vehicle>>(
+            this.apiUrl,
+            vehicle,
+            this.httpOptions
+        );
     }
 }
